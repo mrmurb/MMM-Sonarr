561b3f7ada908a067e96f693476ba255c195a3b2
max <max@ig4.se>
2017-04-26 09:14:47 +0200

    Fixes to follow eslint of MagicMirror.

--- MMM-Sonarr.js
+++ MMM-Sonarr.js
@@ -2,134 +2,134 @@
  * Created by max on 4/21/17.
  */
 Module.register("MMM-Sonarr", {
-    defaults: {
-        apiBase: "",
-        apiKey: "",
-        fade: true,
-        updateInterval: 5 * 50 * 1000,
-        initialLoadDelay: 0,
-        maximumEntries: 5,
-        calendar: {
-            num: 1,
-            type: "month" // days|months|years
-        },
-        history: {
-            sortKey: "date", // date|series.title
-            sortDir: "desc"
-        }
-    },
+	defaults: {
+		apiBase: "",
+		apiKey: "",
+		fade: true,
+		updateInterval: 5 * 50 * 1000,
+		initialLoadDelay: 0,
+		maximumEntries: 5,
+		calendar: {
+			num: 1,
+			type: "month" // days|months|years
+		},
+		history: {
+			sortKey: "date", // date|series.title
+			sortDir: "desc"
+		}
+	},
 
-    getStyles: function () {
-        return ["MMM-Sonarr.css"];
-    },
+	getStyles: function () {
+		return ["MMM-Sonarr.css"];
+	},
 
-    getScripts: function () {
-        return ["moment.js"];
-    },
+	getScripts: function () {
+		return ["moment.js"];
+	},
 
-    start: function () {
-        Log.info("Starting module: " + this.name);
+	start: function () {
+		Log.info("Starting module: " + this.name);
 
-        this.loaded = false;
-        this.sendSocketNotification("CONFIG", this.config);
-    },
+		this.loaded = false;
+		this.sendSocketNotification("CONFIG", this.config);
+	},
 
-    socketNotificationReceived: function (notification, payload) {
-        Log.log(this.name + " received notification");
-        if(notification === "RECORDS") {
-            this.records = payload;
-            this.loaded = true;
-            this.updateDom();
-        }
-    },
+	socketNotificationReceived: function (notification, payload) {
+		Log.log(this.name + " received notification");
+		if(notification === "RECORDS") {
+			this.records = payload;
+			this.loaded = true;
+			this.updateDom();
+		}
+	},
 
-    getDom: function () {
-        let wrapper = document.createElement("div");
+	getDom: function () {
+		let wrapper = document.createElement("div");
 
-        if(!this.loaded) {
-            wrapper.innerHTML = "Fetching records...";
-            wrapper.className = "dimmed light small";
-            return wrapper;
-        }
+		if(!this.loaded) {
+			wrapper.innerHTML = "Fetching records...";
+			wrapper.className = "dimmed light small";
+			return wrapper;
+		}
 
-        let calendarTable = document.createElement("table");
-        let calendarHeader = document.createElement("header");
-        calendarTable.className = "small calendar";
-        calendarHeader.innerHTML = "Upcoming";
+		let calendarTable = document.createElement("table");
+		let calendarHeader = document.createElement("header");
+		calendarTable.className = "small calendar";
+		calendarHeader.innerHTML = "Upcoming";
 
-        let historyTable = document.createElement("table");
-        let historyHeader = document.createElement("header");
-        historyTable.className = "small history";
-        historyHeader.innerHTML = "History";
+		let historyTable = document.createElement("table");
+		let historyHeader = document.createElement("header");
+		historyTable.className = "small history";
+		historyHeader.innerHTML = "History";
 
 
 
-        for(let r in this.records.calendar) {
-            if(r >= this.config.maximumEntries) break;
+		for(let r in this.records.calendar) {
+			if(r >= this.config.maximumEntries) {break;}
 
-            let record = this.records.calendar[r];
+			let record = this.records.calendar[r];
 
-            let row = document.createElement("tr");
-            calendarTable.appendChild(row);
+			let row = document.createElement("tr");
+			calendarTable.appendChild(row);
 
-            let airTimeCell = document.createElement("td");
-            airTimeCell.className = "airtime";
-            airTimeCell.innerHTML = record.airdate;
-            row.appendChild(airTimeCell);
+			let airTimeCell = document.createElement("td");
+			airTimeCell.className = "airtime";
+			airTimeCell.innerHTML = record.airdate;
+			row.appendChild(airTimeCell);
 
-            let seriesTitleCell = document.createElement("td");
-            seriesTitleCell.className = "seriestitle";
-            seriesTitleCell.innerHTML = record.seriesTitle;
-            row.appendChild(seriesTitleCell);
+			let seriesTitleCell = document.createElement("td");
+			seriesTitleCell.className = "seriestitle";
+			seriesTitleCell.innerHTML = record.seriesTitle;
+			row.appendChild(seriesTitleCell);
 
-            let episodeTitleCell = document.createElement("td");
-            episodeTitleCell.className = "episodetitle";
-            episodeTitleCell.innerHTML = record.episodeTitle;
-            row.appendChild(episodeTitleCell);
+			let episodeTitleCell = document.createElement("td");
+			episodeTitleCell.className = "episodetitle";
+			episodeTitleCell.innerHTML = record.episodeTitle;
+			row.appendChild(episodeTitleCell);
 
-        }
+		}
 
-        for(let r in this.records.history) {
-            if(r >= this.config.maximumEntries) break;
+		for(let r in this.records.history) {
+			if(r >= this.config.maximumEntries) {break;}
 
-            let record = this.records.history[r];
+			let record = this.records.history[r];
 
-            let row = document.createElement("tr");
-            historyTable.appendChild(row);
+			let row = document.createElement("tr");
+			historyTable.appendChild(row);
 
-            let airTimeCell = document.createElement("td");
-            airTimeCell.className = "airtime";
-            airTimeCell.innerHTML = record.airDate;
-            row.appendChild(airTimeCell);
+			let airTimeCell = document.createElement("td");
+			airTimeCell.className = "airtime";
+			airTimeCell.innerHTML = record.airDate;
+			row.appendChild(airTimeCell);
 
-            let seriesTitleCell = document.createElement("td");
-            seriesTitleCell.className = "seriestitle";
-            seriesTitleCell.innerHTML = record.seriesTitle;
-            row.appendChild(seriesTitleCell);
+			let seriesTitleCell = document.createElement("td");
+			seriesTitleCell.className = "seriestitle";
+			seriesTitleCell.innerHTML = record.seriesTitle;
+			row.appendChild(seriesTitleCell);
 
-            let episodeTitleCell = document.createElement("td");
-            episodeTitleCell.className = "episodetitle";
-            episodeTitleCell.innerHTML = record.episodeTitle;
-            row.appendChild(episodeTitleCell);
-        }
+			let episodeTitleCell = document.createElement("td");
+			episodeTitleCell.className = "episodetitle";
+			episodeTitleCell.innerHTML = record.episodeTitle;
+			row.appendChild(episodeTitleCell);
+		}
 
-        wrapper.appendChild(calendarHeader);
-        wrapper.appendChild(calendarTable);
-        wrapper.appendChild(historyHeader);
-        wrapper.appendChild(historyTable);
-        return wrapper;
-    },
-    
-    scheduleUpdate: function (delay) {
-        let next = this.config.updateInterval;
-        if (typeof delay !== 'undefined' && delay >= 0) {
-            next = delay;
-        }
+		wrapper.appendChild(calendarHeader);
+		wrapper.appendChild(calendarTable);
+		wrapper.appendChild(historyHeader);
+		wrapper.appendChild(historyTable);
+		return wrapper;
+	},
 
-        let self = this;
-        clearTimeout(this.updateTimer);
-        this.updateTimer = setTimeout(function() {
-            self.updateDom();
-        }, next);
-    }
+	scheduleUpdate: function (delay) {
+		let next = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			next = delay;
+		}
+
+		let self = this;
+		clearTimeout(this.updateTimer);
+		this.updateTimer = setTimeout(function() {
+			self.updateDom();
+		}, next);
+	}
 });
--- node_helper.js
+++ node_helper.js
@@ -2,122 +2,122 @@
  * Created by max on 4/21/17.
  */
 
-const NodeHelper = require('node_helper');
-const unirest = require('unirest');
-const moment = require('moment');
+const NodeHelper = require("node_helper");
+const unirest = require("unirest");
+const moment = require("moment");
 
 module.exports = NodeHelper.create({
-    start: function () {
-        console.log("Starting node_helper for module: " + this.name);
-        this.started = false;
-    },
+	start: function () {
+		console.log("Starting node_helper for module: " + this.name);
+		this.started = false;
+	},
 
     // Receive notification
-    socketNotificationReceived: function (notification, payload) {
-        console.log("node_helper for " + this.name + " received socket notification: " + notification + " - Payload: " + payload);
+	socketNotificationReceived: function (notification, payload) {
+		console.log("node_helper for " + this.name + " received socket notification: " + notification + " - Payload: " + payload);
 
-        if(notification === "CONFIG" && this.started === false) {
-            this.config = payload;
-            this.started = true;
-            this.scheduleUpdate(this.config.initialLoadDelay);
-        }
-    },
+		if(notification === "CONFIG" && this.started === false) {
+			this.config = payload;
+			this.started = true;
+			this.scheduleUpdate(this.config.initialLoadDelay);
+		}
+	},
 
-    updateRecords: function () {
-        let self = this;
-        let response = [];
+	updateRecords: function () {
+		let self = this;
+		let response = [];
 
-        unirest.get(self.getUrl("calendar")).send().end(function (r) {
-            if (r.error) {
-                console.log(self.name + " : " + r.error);
+		unirest.get(self.getUrl("calendar")).send().end(function (r) {
+			if (r.error) {
+				console.log(self.name + " : " + r.error);
                 //TODO add update call
-            } else {
+			} else {
                 // console.log("body: ", JSON.stringify(r.body));
                 // self.parseRecords(r.body);
-                response.calendar = r.body;
+				response.calendar = r.body;
 
-                unirest.get(self.getUrl("history")).send().end(function (r) {
-                    if (r.error) {
-                        console.log(self.name + " : " + r.error);
-                    } else {
-                        response.history = r.body.records;
-                        self.parseRecords(response);
-                    }
-                })
-            }
-        });
-    },
+				unirest.get(self.getUrl("history")).send().end(function (r) {
+					if (r.error) {
+						console.log(self.name + " : " + r.error);
+					} else {
+						response.history = r.body.records;
+						self.parseRecords(response);
+					}
+				})
+			}
+		});
+	},
 
-    parseRecords: function (data) {
-        let records = {
-            calendar: [],
-            history: []
-        };
+	parseRecords: function (data) {
+		let records = {
+			calendar: [],
+			history: []
+		};
 
         // Handle upcoming records, calendar.
-        for (let i in data.calendar) {
-            let record = data.calendar[i];
-            let airDate = record.airDate;
-            let seriesTitle = record.series.title;
-            let episodeTitle = record.title;
+		for (let i in data.calendar) {
+			let record = data.calendar[i];
+			let airDate = record.airDate;
+			let seriesTitle = record.series.title;
+			let episodeTitle = record.title;
 
-            records.calendar.push({
-                airdate: moment(airDate).format("D/M"),
-                seriesTitle: seriesTitle.replace(/ *\([^)]*\) */g, "").trim(),
-                episodeTitle: episodeTitle
-            });
-        }
+			records.calendar.push({
+				airdate: moment(airDate).format("D/M"),
+				seriesTitle: seriesTitle.replace(/ *\([^)]*\) */g, "").trim(),
+				episodeTitle: episodeTitle
+			});
+		}
 
         // Handle past records, history
 
-        for (let i in data.history) {
-            let record = data.history[i];
-            if(record.eventType === "downloadFolderImported") {
-                let airDate = record.episode.date;
-                let seriesTitle = record.series.title;
-                let episodeTitle = record.episode.title;
+		for (let i in data.history) {
+			let record = data.history[i];
+			if(record.eventType === "downloadFolderImported") {
+				let airDate = record.episode.date;
+				let seriesTitle = record.series.title;
+				let episodeTitle = record.episode.title;
 
-                records.history.push({
-                    airDate: moment(airDate).format("D/M"),
-                    seriesTitle: seriesTitle.replace(/ *\([^)]*\) */g, "").trim(),
-                    episodeTitle: episodeTitle
-                });
-            }
-        }
+				records.history.push({
+					airDate: moment(airDate).format("D/M"),
+					seriesTitle: seriesTitle.replace(/ *\([^)]*\) */g, "").trim(),
+					episodeTitle: episodeTitle
+				});
+			}
+		}
 
-        this.scheduleUpdate(this.config.updateInterval);
-        this.sendSocketNotification("RECORDS", records);
-    },
+		this.scheduleUpdate(this.config.updateInterval);
+		this.sendSocketNotification("RECORDS", records);
+	},
 
-    getUrl: function (endpoint) {
-        let url = this.config.apiBase;
-        url += endpoint;
-        url += "?apiKey=" + this.config.apiKey;
+	getUrl: function (endpoint) {
+		let url = this.config.apiBase;
+		url += endpoint;
+		url += "?apiKey=" + this.config.apiKey;
 
-        if(endpoint === "calendar") {
-            let date = moment();
-            url += "&start=" + date.add(1, "days").format("YYYY-MM-DD");
-            url += "&end=" + date.add(this.config.calendar.num, this.config.calendar.type).format("YYYY-MM-DD");
-        }
+		if(endpoint === "calendar") {
+			let date = moment();
+			url += "&start=" + date.add(1, "days").format("YYYY-MM-DD");
+			url += "&end=" + date.add(this.config.calendar.num, this.config.calendar.type).format("YYYY-MM-DD");
+		}
 
-        if(this.config.apiEndpoint === "history") {
-            url += "&sortKey=" + this.config.history.sortKey;
-            url += "&sortDir=" + this.config.history.sortDir;
-        }
+		if(this.config.apiEndpoint === "history") {
+			url += "&sortKey=" + this.config.history.sortKey;
+			url += "&sortDir=" + this.config.history.sortDir;
+		}
 
-        return url;
-    },
+		return url;
+	},
 
-    scheduleUpdate: function (delay) {
-        let next = this.config.updateInterval;
-        if (typeof delay !== 'undefined' && delay >= 0) {
-            next = delay;
-        }
+	scheduleUpdate: function (delay) {
+		let next = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			next = delay;
+		}
 
-        let self = this;
-        clearTimeout(this.updateTimer);
-        this.updateTimer = setTimeout(function() {
-            self.updateRecords();
-        }, next);
-    }
+		let self = this;
+		clearTimeout(this.updateTimer);
+		this.updateTimer = setTimeout(function() {
+			self.updateRecords();
+		}, next);
+	}
 });
